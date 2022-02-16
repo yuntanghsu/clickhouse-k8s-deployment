d7841c04db58e1b5058ee759fcb08680f2b51a5a
Yanjun Zhou <zhouya@vmware.com>
2022-02-16 15:48:19 -0800

    Add deployment scripts and data schema
    Signed-off-by: Yanjun Zhou <zhouya@vmware.com>

---
+++ deployment/start-flow-visibility.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Copyright 2022 Antrea Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+start_flow_visibility() {
+  echo "=== Starting Flow Visibility ==="
+  # install Clickhouse operator
+  kubectl apply -f https://raw.githubusercontent.com/Altinity/clickhouse-operator/master/deploy/operator/clickhouse-operator-install-bundle.yaml
+  kubectl create namespace flow-visibility
+  kubectl create configmap clickhouse-mounted-configmap -n flow-visibility --from-file=${THIS_DIR}/datasources/
+  kubectl apply -f ${THIS_DIR}/flow-visibility.yaml -n flow-visibility
+
+  echo "=== Waiting for Clickhouse and Grafana to be ready ==="
+  sleep 10
+  kubectl wait --for=condition=ready pod -l app=clickhouse-operator -n kube-system --timeout=60s
+  kubectl wait --for=condition=ready pod -l app=clickhouse -n flow-visibility --timeout=60s
+}
+
+start_flow_visibility
