489ba591cf9ebb8e972000e674dceed9f5ac6b6f
Yanjun Zhou <zhouya@vmware.com>
2022-05-05 13:07:18 -0700

    Add cluster deployment drafts
    Signed-off-by: Yanjun Zhou <zhouya@vmware.com>

--- zookeeper-3-nodes-create.sh
+++ zookeeper-3-nodes-create.sh
@@ -1,8 +1,10 @@
+
+   
 #!/bin/bash
 
-ZK_NAMESPACE="${ZK_NAMESPACE:-zoo1ns}"
+ZK_NAMESPACE="${ZK_NAMESPACE:-zoo3ns}"
 
 CUR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 kubectl create namespace "${ZK_NAMESPACE}"
-kubectl --namespace="${ZK_NAMESPACE}" apply -f "${CUR_DIR}/zookeeper-1-node.yaml"
+kubectl --namespace="${ZK_NAMESPACE}" apply -f "${CUR_DIR}/zookeeper3.yaml"
